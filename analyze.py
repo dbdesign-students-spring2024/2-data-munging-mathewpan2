cb3acbd101a8d86678aa1402d4393a98399ccf0d
mathewpan2 <mathew.pan2@gmail.com>
2024-02-13 02:48:58 -0500

    finished instructions

--- analyze.py
+++ analyze.py
@@ -1,1 +1,27 @@
 # Place code below to do the analysis part of the assignment.
+import csv
+import os
+
+analyze = {}
+with open(os.path.join("data", 'cleaned_data.csv'), 'r') as file:
+    reader = csv.reader(file)
+    next(reader)
+    for row in reader:
+        if len(row[13]) > 0:
+            analyze[row[0]] = row[13]
+
+avg = 0
+year = 1880
+index = 0
+for k, v in analyze.items():
+    if index < 10:
+        avg += float(v)
+        index += 1
+    else:
+        avg = avg / 10
+        print(f"{year} to {year + 9}: {avg:.2f}")
+        year += 10
+        avg = float(v)
+        index = 1
+
+                    
--- munge.py
+++ munge.py
@@ -1,1 +1,39 @@
 # Place code below to do the munging part of this assignment.
+import os
+
+
+
+data = []
+
+index = 1
+heading = False
+with open(os.path.join("data", 'GLB.Ts+dSST.txt'), "r") as reader:
+    for line in reader:
+        line = line.strip()
+        if index > 7:
+            if len(line) == 0:
+                continue
+            if line.startswith("Year"):
+                if not heading: # do once if no heading yet
+                    data.append(','.join(line.split()))
+                    heading = True
+                else:
+                    continue
+            else: # data 
+                if line.startswith('Divide'):
+                    break
+                temp = []
+                for item in line.split():
+                    if item.startswith("*"):
+                        temp.append("")
+                    elif int(item) < 1000:
+                        temp.append(format((float(item) / 100) * 9/5 , '.1f'))
+                    else:
+                        temp.append(item)
+                data.append(','.join(temp))
+        index += 1
+
+with open(os.path.join("data", 'cleaned_data.csv'), "w") as writer:
+    for line in data:
+        writer.write(line + "\n")
+
